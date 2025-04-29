157509c8cbc3d7aabdda049e59f58b3cedc84119
jhonbg <jhonbg7@gmail.com>
2025-04-29 12:45:32 -0500

    hasta la clase 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  welcome = 'Hola!';
-  tasks = [
-    'Instalar el Angular CLI',
-    'Crear proyecto',
-    'Crear componentes',
-    'Crear servicio'
-  ]
+  title = 'todo-app';
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+  welcome = 'Hola!';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
+    'Crear servicio'
+  ]
+  name = 'Juan';
+  age = 26;
+  disabled = true
+  img = 'https://w3schools.com/howto/img_avatar.png'
 
+  person = {
+    name: 'Jhon',
+    age: 18,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
 }
